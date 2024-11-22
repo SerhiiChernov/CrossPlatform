4ab497b61443c4aa5976afcb364eb707bb1b8dd6
SerhiiChernov <chernovsrg@gmail.com>
2024-11-22 12:56:09 +0200

    lab 5

--- LabLibrary/LabRunner.cs
+++ LabLibrary/LabRunner.cs
@@ -4,7 +4,8 @@ using Lab3;
 
 namespace LabsLibrary
 {
-    public class LabRunner
+    
+public class LabRunner
     {
         public void RunLab1(string inputFilePath, string outputFilePath)
         {
@@ -14,6 +15,21 @@ namespace LabsLibrary
             File.WriteAllText(outputFilePath, res.ToString());
 
             Console.WriteLine("Data successfully written to the output file.");
+        }
+        public string RunLab1(Stream inputFileStream)
+        {
+            SoldierSeparator soldierSeparator = new SoldierSeparator();
+            using (var reader = new StreamReader(inputFileStream)) 
+            {
+                int res = soldierSeparator.Execute(Convert.ToInt32(reader.ReadToEnd().Trim()));
+                return res.ToString();
+            }
+            //int res = soldierSeparator.Execute(Convert.ToInt32(File.ReadAllText(inputFilePath).Trim()));
+
+            //File.WriteAllText(outputFilePath, res.ToString());
+
+            //Console.WriteLine("Data successfully written to the output file.");
+            //return res.ToString();
         }
 
         public void RunLab2(string inputFilePath, string outputFilePath)
@@ -35,6 +51,30 @@ namespace LabsLibrary
             File.WriteAllText(outputFilePath, maxSum.ToString());
 
             Console.WriteLine("Data successfully written to the output file.");
+        }
+
+        public string RunLab2(Stream inputFileStream)
+        {
+            string[] input;
+            using (var reader = new StreamReader(inputFileStream))
+            {
+                input = reader.ReadToEnd().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            }
+
+            var firstLine = input[0].Split();
+            int N = int.Parse(firstLine[0]);
+            int K = int.Parse(firstLine[1]);
+
+            // Створюємо матрицю на основі вхідних даних
+            Matrix matrix = new Matrix(N, input);
+
+            // Створюємо об'єкт для пошуку максимального шляху
+            Lab2.MatrixPathFinder pathFinder = new Lab2.MatrixPathFinder(matrix, K);
+
+            // Знаходимо максимальну суму
+            int maxSum = pathFinder.FindMaxSum();
+
+            return maxSum.ToString();
         }
 
         public void RunLab3(string inputFilePath, string outputFilePath)
@@ -71,5 +111,34 @@ namespace LabsLibrary
 
             Console.WriteLine("Data successfully written to the output file.");
         }
+        public string RunLab3(Stream inputFileStream)
+        {
+            List<Vector> points = new List<Vector>();
+            int n;
+
+            using (var reader = new StreamReader(inputFileStream))
+            {
+                // Зчитуємо кількість точок
+                n = int.Parse(reader.ReadLine());
+
+                // Зчитуємо точки
+                for (int i = 0; i < n; i++)
+                {
+                    var input = reader.ReadLine().Split();
+                    int x = int.Parse(input[0]);
+                    int y = int.Parse(input[1]);
+                    points.Add(new Vector(x, y));
+                }
+            }
+
+            Lab3.MatrixPathFinder pathFinder = new Lab3.MatrixPathFinder(points);
+
+            // Знаходимо найкоротший шлях
+            var result = pathFinder.FindShortestPath();
+
+            return result.ToString(); 
+        }
+
     }
+    
 }
---
+++ Labs/Lab5/Controllers/ProgramController.cs
@@ -0,0 +1,94 @@
+﻿using LabsLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Lab5.Controllers
+{
+    public class ProgramController : Controller
+    {
+        private readonly LabRunner _labRunner;
+
+        public ProgramController()
+        {
+            _labRunner = new LabRunner();
+        }
+
+        
+        [Authorize]
+        [HttpGet]
+        public IActionResult RunLab1()
+        {
+            return View("~/Views/Labs/RunLab1.cshtml");
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult RunLab1(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    file.CopyTo(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin); 
+
+                    var result = _labRunner.RunLab1(memoryStream);
+
+                    ViewData["Result"] = result;
+                }
+            }
+
+            return View("~/Views/Labs/RunLab1.cshtml");
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult RunLab2()
+        {
+            return View("~/Views/Labs/RunLab2.cshtml");
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult RunLab2(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    file.CopyTo(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+
+                    var result = _labRunner.RunLab2(memoryStream);
+
+                    ViewData["Result"] = result;
+                }
+            }
+
+            return View("~/Views/Labs/RunLab2.cshtml");
+        }
+        [Authorize]
+        [HttpGet]
+        public IActionResult RunLab3()
+        {
+            return View("~/Views/Labs/RunLab3.cshtml");
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult RunLab3(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    file.CopyTo(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+
+                    var result = _labRunner.RunLab3(memoryStream);
+                    ViewData["Result"] = result;
+                }
+            }
+
+            return View("~/Views/Labs/RunLab3.cshtml");
+        }
+    }
+}
